25506da4c3e9deb25318f9b718680ad280992e1b
thegr8od <zzjoon97@naver.com>
2024-03-29 15:07:53 +0900

    240329

---
+++ 1206View.py
@@ -0,0 +1,21 @@
+for i in range(1,11):
+    n = int(input())
+    height = list(map(int, input().strip().split(" ")))
+    cnt = 0
+    for j in range(n):
+        if j == 0:
+            if height[j] > height[j+1] and height[j] > height[j+2]:
+                cnt += height[j] - max(height[j+1], height[j+2])
+        if j == 1:
+            if height[j] > height[j+1] and height[j] > height[j+2] and height[j]>height[j-1]:
+                cnt += height[j] - max(height[j+1], height[j+2], height[j-1])
+        if j == n-2:
+            if height[j] > height[j-2] and height[j] > height[j-1] and height[j]>height[j+1]:
+                cnt += height[j] - max(height[j-2], height[j-1], height[j+1])
+        if j == n-1:
+             if height[j] > height[j-2] and height[j] > height[j-1]:
+                cnt += height[j] - max(height[j-2], height[j-1]) 
+        else:
+            if height[j] > height[j-1] and height[j] > height[j-2] and height[j] > height[j+1] and height[j] > height[j+2]:
+                cnt += height[j] - max(height[j-2], height[j-1],height[j+1], height[j+2]) 
+    print("#"+str(i),+cnt)                   
