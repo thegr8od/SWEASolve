124b1d46be06f46909d18ee5cf10c936435caab6
thegr8od <zzjoon97@naver.com>
2024-05-12 13:30:29 +0900

    240512

---
+++ 3304최장 공통 부분 수열.py
@@ -0,0 +1,19 @@
+t = int(input())
+
+for tc in range(1,t+1):
+    a,b = input().split()
+    dp = [[0] * (len(a) + 1) for _ in range(len(b) +1)]
+
+    for i in range(1,len(dp)):
+        for j in range(1, len(dp[i])) :
+            if a[j-1] == b[i-1] :
+                dp[i][j] = dp[i-1][j-1] +1
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+    
+    print("#{} {}".format(tc, dp[-1][-1]))            
+        
+        
+        
+        
+    
